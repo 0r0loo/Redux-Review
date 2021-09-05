29b9616d0d5c9c802d9f3c273d11a45f14dfa88f
chotaegyu <chotg5592@gmail.com>
2021-09-05 22:44:50 +0900

    2. redux 용어로 바꾸기

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 import { createStore } from './redux.js';
 
 // 각 컴포넌트에서 상태를 어떻게 바꿀것인지 정의하는 함수
-function updater(state, data) {
-  return (state = data);
+function reducer(state, action) {
+  if (action.type === 'increment') {
+    state = {
+      ...state,
+      value: action.payload.value,
+    };
+  }
+  return state;
 }
 
 // 전역 상태 만들기
-const store = createStore(updater);
+const store = createStore(reducer);
 // 상태를 설정하기전
 console.log(store.getState());
 
 // 상태 업데이트
-store.doUpdate('Update state');
+store.dispatch({ type: 'increment', payload: { value: 1 } });
 // 상태를 바꾼 후
 console.log(store.getState());
